a28b3ae7be6639c5d2d52aa2e640f42a877b6106
Al-Amin Hossain <alaminnishat@gmail.com>
2023-06-06 14:43:28 +0600

    chakra installed & react react-dom updated

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { NavigationMenu } from "@shopify/app-bridge-react";
+
 import Routes from "./Routes";
 
 import {
@@ -13,21 +12,12 @@ export default function App() {
   // Any .tsx or .jsx files in /pages will become a route
   // See documentation for <Routes /> for more info
   const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
-  const { t } = useTranslation();
 
   return (
     <PolarisProvider>
       <BrowserRouter>
         <AppBridgeProvider>
           <QueryProvider>
-            <NavigationMenu
-              navigationLinks={[
-                {
-                  label: t("NavigationMenu.pageName"),
-                  destination: "/pagename",
-                },
-              ]}
-            />
             <Routes pages={pages} />
           </QueryProvider>
         </AppBridgeProvider>
