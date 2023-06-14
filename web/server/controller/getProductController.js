f99ff337cc8bcb5b28dc5022be85e819f861b286
Al-Amin Hossain <alaminnishat@gmail.com>
2023-06-14 18:37:32 +0600

    few changes

--- web/server/controller/getProductController.js
+++ web/server/controller/getProductController.js
@@ -3,33 +3,42 @@ import shopify from "../../shopify.js";
 // graphql query
 const getProductQuery = `#graphql
 query Products($first:Int!, $query:String){
-products(first:$first, query:$query){
-  edges {
-node {
-  id
-  handle
-  variants(first: 10) {
+  products(first: $first, query: $query) {
     edges {
       node {
-        sku
+        id
+        handle
+        variants(first: 10) {
+          edges {
+            node {
+              sku
+              id
+              price
+              image {
+                src
+              }
+            }
+          }
+        }
+        featuredImage {
+          src
+        }
       }
     }
+    
   }
-}
-}
-}
 }`;
 
 export const getProductController = async (req, res, next) => {
   const session = res.locals.shopify.session;
-  console.log(session);
+  const query = req.query.search;
   const client = new shopify.api.clients.Graphql({ session });
   const productsData = await client.query({
     data: {
       query: getProductQuery,
       variables: {
         first: 10,
-        query: `sku:f3*`,
+        query: `sku:${query}*`,
       },
     },
   });
